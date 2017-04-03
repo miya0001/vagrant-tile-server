62151dc04f10da46da867f27fe25562059b8e009
miya0001 <miya@wpist.me>
2017-04-04 03:20:53 +0900

    some fix

--- spec/default/defaut_spec.rb
+++ spec/default/defaut_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe port(6789) do
+  it { should be_listening }
+end
+
 describe package('apache2') do
   it { should be_installed }
 end
@@ -32,6 +41,8 @@ describe command('curl --dump-header - --head http://localhost/map/0/0/0.png'), 
   its(:stdout) { should contain('Content-Type: image/png') }
 end
 
-describe port(80) do
-  it { should be_listening }
+describe command('curl -L --dump-header - http://localhost:6789/'), :sudo => false do
+  its(:stdout) { should contain('200 OK') }
+  its(:stdout) { should contain('Content-Type: text/html') }
+  its(:stdout) { should contain('<title>Kosmtik</title>') }
 end
